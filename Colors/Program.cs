bed10bda7df5f5cc6df337403748f150228c02f8
Julia Aasmaa <my_e_10@hotmail.com>
2021-11-09 17:13:42 +0200

    add horoscope

---
+++ Colors/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Colors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //programm küsib kasutajal sisestada tema lemmikväravi
+            //programm loeb failist personality.txt faili sisu, kontrollib, kas kasutaja poolt värv on failis olemas
+            //pogramm kuvab kasutajale tema poolt sisestatud värvi krijelduse, ehk mida see värv kasutaja kohta räägib
+        }
+    }
+}
---
+++ Horoscope/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.IO;
+
+namespace Horoscope
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your horoscope sign:");
+            string userSign = Console.ReadLine();
+            Console.WriteLine(GetHoroscope(userSign));
+            
+        }
+        //a function to open and read the file
+        public static string[] ReadHoroScope()
+        {
+            //absolute file path
+            string filePath = @"C:\Users\opilane\samples\horoscope.txt";
+            string[] dataFromFile = File.ReadAllLines(filePath);            
+            return dataFromFile;
+        }
+
+        public static string GetHoroscope(string userInput)
+        {
+            string[] horoscope = ReadHoroScope();
+            string todayHoroscope = "";
+
+            foreach(string element in horoscope)
+            {
+                if (element.Contains(userInput))
+                {
+                    todayHoroscope = element;
+                }
+            }
+
+            return todayHoroscope;
+        }
+
+
+
+
+    }
+}
